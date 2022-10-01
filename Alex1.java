19998ca1e6fd5d1f70ac904e1e7da36ed6c6473b
syabdull <syab5050@gmail.com>
2022-10-01 08:53:43 +0530

    initial commit

---
+++ Alex1.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+public class Alex1 {
+     
+     
+    public static  void  main(String[]args){
+         ArrayList<Integer>al=new ArrayList<>();
+            al.add(10);
+            System.out.println(al);
+
+
+    }
+    
+}
